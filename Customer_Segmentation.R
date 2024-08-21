6f116d4ed6fc3520570470992fa32546155b976f
ShrishtiG02 <81442389+ShrishtiG02@users.noreply.github.com>
2024-08-21 16:33:58 +0530

    misc

--- Customer_Segmentation.R
+++ Customer_Segmentation.R
@@ -1,4 +1,4 @@
-customer_data=read.csv("C:/Users/sauranh sangle/Desktop/New folder/Mall_Customers.csv")
+customer_data=read.csv("C:/Users/shrishti/Desktop/New folder/Mall_Customers.csv")
 str(customer_data)
 
 names(customer_data)
